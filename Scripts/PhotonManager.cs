3ef9b901ff79a013fc441f6fbe1345770410a502
Raemin KANG <foals18@gmail.com>
2018-05-06 16:40:58 +0900

    PhotonCloud 자체 지원 팀 설정 내용 변경

--- Scripts/PhotonManager.cs
+++ Scripts/PhotonManager.cs
@@ -2,29 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhotonManager : MonoBehaviour
+public class PhotonManager : Photon.MonoBehaviour
 {
+    public static PhotonView ScenePhotonView;
+    //public static int staticTeamNum = 0;
     public bool leaveCheck = false;
     public string playerPrefabName = "US_Marine";
     public GameObject menuPos = null;
     int teamNumView = 0;
     int ptNumView = 0;
-    void Start()
+    string playerCheck = "공백";
+    PlayerState playerState = null;
+    PhotonView pv = null;
+    string serverState = "0";
+    public List<string> playerList = new List<string>();
+
+    private void Start()
     {
+        ScenePhotonView = this.GetComponent<PhotonView>();
     }
+
     void OnJoinedRoom()
     {
         Debug.Log("룸접속");
         CreatePlayer();
+        //ScenePhotonView.RPC("otherCreatePlayer", PhotonTargets.All, null);
         Camera.main.GetComponent<CameraControl>().enabled = true;
+        playerList.Add(PhotonNetwork.playerName);
     }
 
     void CreatePlayer()
     {
+        GameObject Player = null;
         //캐릭 Spwan 관련
         float pos = Random.Range(700.0f, 800.0f);
-        GameObject Player = PhotonNetwork.Instantiate(this.playerPrefabName, new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
+        //PunTeams.PlayersPerTeam[] playersPerTeams = new PunTeams.PlayersPerTeam[];
+        //PunTeams.PlayersPerTeam[PunTeams.Team.red] = null;
+        //PunTeams.PlayersPerTeam[PunTeams.Team.blue] = null;
+        //PunTeams.Team[] teams = { PunTeams.Team.red, PunTeams.Team.blue };
+
+        if (PunTeams.PlayersPerTeam[PunTeams.Team.red].Count < PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count)
+        {
+            //GameObject player = PhotonNetwork.Instantiate("PlayerRed", spawnPointsRed[PunTeams.PlayersPerTeam[PunTeams.Team.red].Count].position, spawnPointsRed[PunTeams.PlayersPerTeam[PunTeams.Team.red].Count].rotation, 0);
+            //PhotonNetwork.player.SetTeam(PunTeams.Team.red);
+            Player = PhotonNetwork.Instantiate("PlayerRed", new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
+            PhotonNetwork.player.SetTeam(PunTeams.Team.red);
+            //PhotonNetwork.playerList[PhotonNetwork.player.ID - 1].SetTeam(PunTeams.Team.red);
+        }
+        else if (PunTeams.PlayersPerTeam[PunTeams.Team.red].Count > PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count)
+        {
+            Player = PhotonNetwork.Instantiate("PlayerBlue", new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
+            PhotonNetwork.player.SetTeam(PunTeams.Team.blue);
+        }
+        else if (PunTeams.PlayersPerTeam[PunTeams.Team.red].Count == PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count)
+        {
+            Player = PhotonNetwork.Instantiate("PlayerRed", new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
+            PhotonNetwork.player.SetTeam(PunTeams.Team.red);
+        }
+        Player.name = PhotonNetwork.playerName;
+        Debug.Log("이름 : " + Player.name);
         //메인카메라 관련
+        //GameObject Player = PhotonNetwork.Instantiate(this.playerPrefabName, new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
         Camera.main.farClipPlane = 1000.0f;
         Camera.main.transform.SetParent(Player.transform);
         GameObject camPivot = Player.transform.Find("CamPivot").gameObject;
@@ -40,22 +78,110 @@ public class PhotonManager : MonoBehaviour
         PlayerFs.enabled = true;
         CheckCollider PlayerCC = Player.GetComponent<CheckCollider>();
         PlayerCC.enabled = true;
-        PlayerState playerState = Player.GetComponent<PlayerState>();
+        playerState = Player.GetComponent<PlayerState>();
         playerState.enabled = true;
-        PhotonView pv = Player.GetComponent<PhotonView>();
+        pv = Player.GetComponent<PhotonView>();
         Debug.Log("플레이어 ID : " + pv.viewID);
         ptNumView = pv.viewID;
         playerState.TeamNum = (pv.viewID / 1000) % 2 == 0 ? 2 : 1;
+        //staticTeamNum = (pv.viewID / 1000) % 2 == 0 ? 2 : 1;
         teamNumView = playerState.TeamNum;
+        playerCheck = playerState.check;
+        playerState.UserId = PhotonNetwork.playerName;
+        //pv.RPC("TaggedPlayer",PhotonTargets.All, playerState.UserId);
         Rader PlayerRader = Player.GetComponentInChildren<Rader>();
         PlayerRader.enabled = true;
+        Debug.Log("CreatePlayer 실행");
+        serverState = "CreatePlayer 실행";
     }
+
+    [PunRPC]
+    void otherCreatePlayer()
+    {
+        GameObject Player = null;
+        //캐릭 Spwan 관련
+        float pos = Random.Range(700.0f, 800.0f);
+        //PunTeams.PlayersPerTeam[] playersPerTeams = new PunTeams.PlayersPerTeam[];
+        //PunTeams.PlayersPerTeam[PunTeams.Team.red] = null;
+        //PunTeams.PlayersPerTeam[PunTeams.Team.blue] = null;
+        //PunTeams.Team[] teams = { PunTeams.Team.red, PunTeams.Team.blue };
+
+        if (PunTeams.PlayersPerTeam[PunTeams.Team.red].Count < PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count)
+        {
+            //GameObject player = PhotonNetwork.Instantiate("PlayerRed", spawnPointsRed[PunTeams.PlayersPerTeam[PunTeams.Team.red].Count].position, spawnPointsRed[PunTeams.PlayersPerTeam[PunTeams.Team.red].Count].rotation, 0);
+            //PhotonNetwork.player.SetTeam(PunTeams.Team.red);
+            Player = PhotonNetwork.Instantiate("PlayerRed", new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
+            PhotonNetwork.player.SetTeam(PunTeams.Team.red);
+            //PhotonNetwork.playerList[PhotonNetwork.player.ID - 1].SetTeam(PunTeams.Team.red);
+        }
+        else if (PunTeams.PlayersPerTeam[PunTeams.Team.red].Count > PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count)
+        {
+            Player = PhotonNetwork.Instantiate("PlayerBlue", new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
+            PhotonNetwork.player.SetTeam(PunTeams.Team.blue);
+        }
+        else if (PunTeams.PlayersPerTeam[PunTeams.Team.red].Count == PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count)
+        {
+            Player = PhotonNetwork.Instantiate("PlayerRed", new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
+            PhotonNetwork.player.SetTeam(PunTeams.Team.red);
+        }
+        Player.name = PhotonNetwork.playerName;
+        Debug.Log("이름 : " + Player.name);
+        //메인카메라 관련
+        //GameObject Player = PhotonNetwork.Instantiate(this.playerPrefabName, new Vector3(pos, 154.7f, pos), Quaternion.identity, 0);
+        Camera.main.farClipPlane = 1000.0f;
+        Camera.main.transform.SetParent(Player.transform);
+        GameObject camPivot = Player.transform.Find("CamPivot").gameObject;
+        Camera.main.transform.position = new Vector3(camPivot.transform.position.x, camPivot.transform.position.y, camPivot.transform.position.z);
+        Camera.main.transform.rotation = new Quaternion(camPivot.transform.rotation.x, camPivot.transform.rotation.y, camPivot.transform.rotation.z, camPivot.transform.rotation.w);
+
+        //플레이어 스크립트 관련
+        CharacterMove PlayerMove = Player.GetComponent<CharacterMove>();
+        PlayerMove.enabled = true;
+        OptionManager PlayerOptionMG = Player.GetComponent<OptionManager>();
+        PlayerOptionMG.enabled = true;
+        FireScript PlayerFs = Player.GetComponent<FireScript>();
+        PlayerFs.enabled = true;
+        CheckCollider PlayerCC = Player.GetComponent<CheckCollider>();
+        PlayerCC.enabled = true;
+        playerState = Player.GetComponent<PlayerState>();
+        playerState.enabled = true;
+        pv = Player.GetComponent<PhotonView>();
+        Debug.Log("플레이어 ID : " + pv.viewID);
+        ptNumView = pv.viewID;
+        playerState.TeamNum = (pv.viewID / 1000) % 2 == 0 ? 2 : 1;
+        //staticTeamNum = (pv.viewID / 1000) % 2 == 0 ? 2 : 1;
+        teamNumView = playerState.TeamNum;
+        playerCheck = playerState.check;
+        playerState.UserId = PhotonNetwork.playerName;
+        //pv.RPC("TaggedPlayer",PhotonTargets.All, PhotonNetwork.playerName);
+        Rader PlayerRader = Player.GetComponentInChildren<Rader>();
+        PlayerRader.enabled = true;
+        Debug.Log("CreatePlayer 실행");
+        serverState = "CreatePlayer 실행";
+    }
+
+    [PunRPC]
+    void TaggedPlayer(string playerID)
+    {
+        Debug.Log("TaggedPlayer() 실행");
+        playerState.UserId = playerID;
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    if (playerCheck != "공백")
+    //    {
+    //        playerCheck = playerState.check;
+    //    }
+    //}
 
     // Update is called once per frame
     void OnGUI()
     {
         GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
-        GUILayout.Label("Team Num : " + teamNumView + " PhotonView ID : " + ptNumView);
+        GUILayout.Label("Team Num : " + teamNumView + " PhotonView ID : " + ptNumView + " 플레이어 접근 : " + playerCheck + " team : " + PhotonNetwork.player.GetTeam());
+        GUILayout.Label("Name : " + PhotonNetwork.playerName);
+        GUILayout.Label("Red : " + PunTeams.PlayersPerTeam[PunTeams.Team.red].Count + " Blue : "+ PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count);
         if (PhotonNetwork.room == null) return; //Only display this GUI when inside a room
 
         if (GUILayout.Button("Leave Room"))
--- Scripts/PlayerState.cs
+++ Scripts/PlayerState.cs
@@ -1,37 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class PlayerState : MonoBehaviour
+public class PlayerState : Photon.PunBehaviour
 {
     public enum PLAYERSTATE
     {
         GROGGY = 0,
         DEAD
     }
-    //Player 생존 확인
-    FireScript fireScript = null;
-    public bool isDead = false;
-    public bool groggy = false;
-    //[HideInInspector] public int TeamNum = 0;
-    public int TeamNum = 1;
-    public bool isGrounded = false;
-    [HideInInspector] public int initHp = 100;
-    [HideInInspector] public int currHp = 0;
-    private MeshRenderer[] renderers;
-    private SkinnedMeshRenderer[] skinRenderers;
+    //Player 스크립트
+    private FireScript fireScript = null;
     private Rigidbody rb = null;
     private PhotonView pv = null;
+    private PhotonManager pm = null;
+    //플레이어 상태
+    public bool isDead = false;
+    public bool groggy = false;
+    public string check = "비 접근";
+
+    //플레이어 속성
+    public string UserId = "Unknown";
+    public bool isGrounded = false;
+    [HideInInspector] public int currHp = 0;
+    [HideInInspector] public int TeamNum = 0;
+    //[HideInInspector] public int initHp = 100;
+
+    //플레이어 비활성화
+    private MeshRenderer[] renderers;
+    private SkinnedMeshRenderer[] skinRenderers;
+
 
     void Awake()
     {
+        //pm = transform.Find("PhotonManager").gameObject.GetComponent<PhotonManager>();
         rb = GetComponent<Rigidbody>();
         fireScript = GetComponent<FireScript>();
-        currHp = initHp;
+        currHp = Constants.initHp;
         pv = GetComponent<PhotonView>();
         renderers = GetComponentsInChildren<MeshRenderer>();
         skinRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
     }
-
+    int myTeamNum = 0;
+    int otherTeamNum = 0;
+    string otherName = "미상";
+    string state = "미상";
     private void OnCollisionEnter(Collision other)
     {
         int collisionLayer = other.gameObject.layer;
@@ -58,15 +70,44 @@ public class PlayerState : MonoBehaviour
         }
         else if (collisionLayer == LayerMask.NameToLayer("Player") && this.gameObject != other.gameObject)
         {
-            PlayerState ps = other.gameObject.GetComponent<PlayerState>();
+            //int playerMax = PunTeams.PlayersPerTeam[PunTeams.Team.red].Count + PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count;
+            //for (int i = 0; i < playerMax; i++)
+            //{
+            //    if (PhotonNetwork.playerList[i].name == other.gameObject.name) {
+            //        state = "찾았다.";
+            //    }
+            //}
+            PhotonView otPV = other.transform.GetComponent<PhotonView>();
+            otherTeamNum = otPV.viewID;
+            foreach (PhotonPlayer pl in PhotonNetwork.playerList)
+            {
+                if (pl.ID == otPV.viewID)
+                {
+                    state = "찾았다.";
+                }
+            }
+            //pv.RPC("TaggedPlayer", PhotonTargets.Others, TeamNum);
+            check = "일단 플레이어끼리 접근";
+            //otherName = other.transform.gameObject.name;
+            //other.transform.gameObject.GetPhotonView
+            PlayerState ps = other.transform.gameObject.GetComponent<PlayerState>();
+            otherName = ps.UserId;
+            //PlayerState ps = other.gameObject.GetComponent<PlayerState>();
+            Rigidbody otherRb = other.gameObject.GetComponent<Rigidbody>();
+            myTeamNum = this.TeamNum;
+            //otherTeamNum = ps.TeamNum;
+            //otherTeamNum = other.gameObject.GetComponent<PlayerState>().TeamNum;
             if (ps.TeamNum == this.TeamNum)
             {
+                check = "우리팀 접근 성공";
+                otherRb.isKinematic = false;
+                rb.isKinematic = false;
                 if (ps.groggy)
                 {
                     if (Input.GetKeyDown(KeyCode.F))
                     {
                         ps.currHp = 40;
-                        ps.SetPlayerVisible(true,PLAYERSTATE.GROGGY);
+                        ps.SetPlayerVisible(true, PLAYERSTATE.GROGGY);
                         ps.groggy = false;
                         ps.fireScript.enabled = true;
                         ps.rb.isKinematic = true;
@@ -74,13 +115,44 @@ public class PlayerState : MonoBehaviour
                 }
             }
         }
-        else if (collisionLayer == LayerMask.NameToLayer("Ground")) {
-            Debug.Log("계속찍나?");
+        if (collisionLayer == LayerMask.NameToLayer("Ground") && isGrounded != true)
+        {
             isGrounded = true;
         }
-
     }
 
+    //void OnPhotonPlayerConnected(PhotonPlayer player)
+    //{
+    //    Debug.Log("OnPhotonPlayerConnected: " + player);
+
+    //    // when new players join, we send "who's it" to let them know
+    //    // only one player will do this: the "master"
+    //    pv.RPC("TaggedPlayer", PhotonTargets.Others, PhotonNetwork.playerName);
+    //}
+
+    //[PunRPC]
+    //void TaggedPlayer(int playerID)
+    //{
+    //    TeamNum = playerID;
+    //}
+
+    void OnGUI()
+    {
+        GUILayout.Label("myTeamNum : " + myTeamNum + " otherTeamNum : " + otherTeamNum + " PlayerName : "+ PhotonNetwork.playerName + " otherName : " + otherName );// + "otherPs : " + otherPs.TeamNum);
+        GUILayout.Label("\n");
+        GUILayout.Label("\n");
+        GUILayout.Label("\n");
+        GUILayout.Label("접촉? : "+ check + "상태 : " + state + " 내 ID : " + UserId);
+    }
+
+    //private void OnCollisionExit(Collision other)
+    //{
+    //    int collisionLayer = other.gameObject.layer;
+    //    if (collisionLayer == LayerMask.NameToLayer("Player") && this.gameObject != other.gameObject)
+    //    {
+    //        check = "비 접근";
+    //    }
+    //}
     void SetPlayerVisible(bool isVisible, PLAYERSTATE state)
     {
         if (state == PLAYERSTATE.GROGGY)
@@ -120,5 +192,4 @@ public class PlayerState : MonoBehaviour
     //        Gun.SetActive(false);
     //    }
     //}
-
 }
