7c2a537aa2d3a77fd1334579b8d48b7a58c5fe53
ParkBK <pbg3949@hanmail.net>
2018-05-14 03:09:12 +0900

    gameSound
    Walk and BGM

---
+++ sound/FootSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootSound : MonoBehaviour
+{
+    CharacterController cc;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        
+    }
+
+    void Update()
+    {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+            GetComponent<AudioSource>().Play();
+
+        }
+    }
+}
---
+++ sound/MainBgm.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainBgm : MonoBehaviour
+{
+    public static AudioSource Mainbgm;
+    void Start()
+    {
+        if (Mainbgm != null)
+            Destroy(gameObject);
+        else
+        {
+            Mainbgm = gameObject.GetComponent<AudioSource>();
+            Mainbgm.mute = false;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+
+}
